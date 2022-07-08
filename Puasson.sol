4176712e01b3a119fa4d5583697190076591dd8a
freeton20 <doctor4891@gmail.com>
2022-07-08 23:38:05 +0300

    10

--- Puasson.sol
+++ Puasson.sol
@@ -3,12 +3,19 @@ pragma ton-solidity >=0.61.2;
 contract Puasson {
 	uint256 public counter = 0;
 	uint256 limit = 1000000;
-	uint256[] public numbers = new uint256[](5);
+	uint256[] public numbers = new uint256[](11);
 
-	uint256 public a11 = 0; // example: 1 1
-	uint256 public a1x1 = 0; //1 x 1
-	uint256 public a1xx1 = 0; //1 x x 1
-	uint256 public a1xxx1 = 0; //1 x x x 1
+	// combinations
+	uint16 public a11 = 0; //1 1
+	uint16 public a1x1 = 0; //1 x 1
+	uint16 public a1xx1 = 0; //1 x x 1
+	uint16 public a1xxx1 = 0; //1 x x x 1
+	uint16 public a1xxxx1 = 0; //1 x x x x 1
+	uint16 public a1xxxxx1 = 0; //1 x x x x x 1
+	uint16 public a1xxxxxx1 = 0; //1 x x x x x x 1
+	uint16 public a1xxxxxxx1 = 0; //1 x x x x x x x 1
+	uint16 public a1xxxxxxxx1 = 0; //1 x x x x x x x x 1
+	uint16 public a1xxxxxxxxx1 = 0; //1 x x x x x x x x x 1
 
 	function run() public {
 		require(counter < limit, 1001, "contract execution is finished");
@@ -25,24 +32,54 @@ contract Puasson {
 			numbers[2],
 			numbers[3],
 			numbers[4],
-			rnd.next(37)
+			numbers[5],
+			numbers[6],
+			numbers[7],
+			numbers[8],
+			numbers[9],
+			numbers[10],
+			rnd.next(36)
 		];
 
-		if (counter > 4) {
-			if (numbers[4] == numbers[3]) {
+		if (counter > 10) {
+			if (numbers[10] == numbers[9]) {
 				a11++;
 			}
 
-			if (numbers[4] == numbers[2]) {
+			if (numbers[10] == numbers[8]) {
 				a1x1++;
 			}
 
-			if (numbers[4] == numbers[1]) {
+			if (numbers[10] == numbers[7]) {
 				a1xx1++;
 			}
 
-			if (numbers[4] == numbers[0]) {
+			if (numbers[10] == numbers[6]) {
 				a1xxx1++;
+			}
+
+			if (numbers[10] == numbers[5]) {
+				a1xxxx1++;
+			}
+
+			if (numbers[10] == numbers[4]) {
+				a1xxxxx1++;
+			}
+
+			if (numbers[10] == numbers[3]) {
+				a1xxxxxx1++;
+			}
+
+			if (numbers[10] == numbers[2]) {
+				a1xxxxxxx1++;
+			}
+
+			if (numbers[10] == numbers[1]) {
+				a1xxxxxxxx1++;
+			}
+
+			if (numbers[10] == numbers[0]) {
+				a1xxxxxxxxx1++;
 			}
 		}
 	}
@@ -51,12 +88,29 @@ contract Puasson {
 		public
 		view
 		returns (
-			uint256,
-			uint256,
-			uint256,
-			uint256
+			uint16,
+			uint16,
+			uint16,
+			uint16,
+			uint16,
+			uint16,
+			uint16,
+			uint16,
+			uint16,
+			uint16
 		)
 	{
-		return (a11, a1x1, a1xx1, a1xxx1);
+		return (
+			a11,
+			a1x1,
+			a1xx1,
+			a1xxx1,
+			a1xxxx1,
+			a1xxxxx1,
+			a1xxxxxx1,
+			a1xxxxxxx1,
+			a1xxxxxxxx1,
+			a1xxxxxxxxx1
+		);
 	}
 }
